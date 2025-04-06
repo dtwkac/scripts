09dd0a0ee87298b94cc5bee228dfcb0e67a9d03d
dtwkac <143578277+dtwkac@users.noreply.github.com>
2025-04-07 00:13:40 +0800

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -3,25 +3,25 @@ sudo apt upgrade -y && \
 sudo apt autoremove -y && \
 sudo apt install -y vim g++ cmake cgdb git wget unzip tmux docker.io language-pack-zh-hans && \
 sudo update-locale LANG=zh_CN.UTF-8 && \
-echo "filetype plugin indent on" >> ~/.vimrc && \
-echo "set expandtab" >> ~/.vimrc && \
-echo "set shiftwidth=4" >> ~/.vimrc && \
+echo 'filetype plugin indent on' >> ~/.vimrc && \
+echo 'set expandtab' >> ~/.vimrc && \
+echo 'set shiftwidth=4' >> ~/.vimrc && \
 echo "au BufReadPost * if line(\"'\\\"\") > 1 && line(\"'\\\"\") <= line(\"$\") | exe \"normal! g'\\\"\" | endif" >> ~/.vimrc && \
-echo "let loaded_matchparen = 1" >> ~/.vimrc && \
-echo "machine github.com" >> ~/.netrc && \
-echo "  login " >> ~/.netrc && \
-echo "  password " >> ~/.netrc && \
-echo "machine api.github.com" >> ~/.netrc && \
-echo "  login " >> ~/.netrc && \
-echo "  password " >> ~/.netrc && \
+echo 'let loaded_matchparen = 1' >> ~/.vimrc && \
+echo 'machine github.com' >> ~/.netrc && \
+echo '  login ' >> ~/.netrc && \
+echo '  password ' >> ~/.netrc && \
+echo 'machine api.github.com' >> ~/.netrc && \
+echo '  login ' >> ~/.netrc && \
+echo '  password ' >> ~/.netrc && \
 echo >> ~/.netrc && \
-echo "machine gitee.com" >> ~/.netrc && \
-echo "  login " >> ~/.netrc && \
-echo "  password " >> ~/.netrc && \
-echo "machine api.gitee.com" >> ~/.netrc && \
-echo "  login " >> ~/.netrc && \
-echo "  password " >> ~/.netrc && \
-echo ${win_id_rsa_pub} >> ~/.ssh/authorized_keys && \
+echo 'machine gitee.com' >> ~/.netrc && \
+echo '  login ' >> ~/.netrc && \
+echo '  password ' >> ~/.netrc && \
+echo 'machine api.gitee.com' >> ~/.netrc && \
+echo '  login ' >> ~/.netrc && \
+echo '  password ' >> ~/.netrc && \
+echo "${win_id_rsa_pub}" >> ~/.ssh/authorized_keys && \
 mkdir ~/tools && \
 cd ~/tools && \
 anaconda_version=2024.02-1 && \
@@ -34,7 +34,7 @@ conda deactivate && \
 git clone https://github.com/dtwkac/yarx.git && \
 cp yarx/yarx.zip ~/tools && \
 rm -rf yarx && \
-echo "" | sha256sum | awk '{print $1}' | xargs -I {} unzip -P {} yarx.zip && \
+echo '' | sha256sum | awk '{print $1}' | xargs -I {} unzip -P {} yarx.zip && \
 rm yarx.zip && \
 cd xray && \
 unzip Xray-linux-64.zip && \
@@ -44,7 +44,7 @@ sed -i '53s/11111//' config.json && \
 sed -i '56s/""/""/' config.json && \
 tmux new -d -s xray && \
 sleep 1 && \
-tmux send -t xray "~/tools/xray/xray" Enter && \
+tmux send -t xray '~/tools/xray/xray' Enter && \
 sleep 1 && \
 cd && \
 echo "alias p='python3'" >> ~/.bash_aliases && \
