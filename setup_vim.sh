8fa33fc587ebb405cd3d27329585953107fbe882
dtwkac <143578277+dtwkac@users.noreply.github.com>
2025-04-07 17:17:49 +0800

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -10,8 +10,7 @@ echo "  elseif has('python3')" >> ~/.vimrc && \
 echo '    py3f /usr/share/vim/addons/syntax/clang-format.py' >> ~/.vimrc && \
 echo '  endif' >> ~/.vimrc && \
 echo 'endfunction' >> ~/.vimrc && \
-echo 'autocmd BufWritePre *.h,*.cc,*.cpp call FormatFile()' >> ~/.vimrc
-
+echo 'autocmd BufWritePre *.h,*.cc,*.cpp call FormatFile()' >> ~/.vimrc && \
 sudo apt install -y curl && \
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
 echo >> ~/.vimrc && \
@@ -19,9 +18,8 @@ echo 'call plug#begin()' >> ~/.vimrc && \
 echo "Plug 'jiangmiao/auto-pairs'" >> ~/.vimrc && \
 echo "Plug 'luochen1990/rainbow'" >> ~/.vimrc && \
 echo "Plug 'ycm-core/YouCompleteMe'" >> ~/.vimrc && \
-echo 'call plug#end()' >> ~/.vimrc
-
-# launch vim, input ":PlugInstall"
+echo 'call plug#end()' >> ~/.vimrc && \
+vim -c "PlugInstall" -c "qa" && \
 sudo apt install -y python3-dev && \
 cd ~/.vim/plugged/YouCompleteMe && \
 python3 install.py --clangd-completer && \
